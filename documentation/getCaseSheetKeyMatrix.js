4285fe7e7ca64757ed5b667fb8a0d37a53e3c2cb
PragyaDas-dev <pragya@spktechnosoft.in>
2025-04-28 18:10:21 +0530

    added api documentation for admin dashboard

--- documentation/getCaseSheetKeyMatrix.js
+++ documentation/getCaseSheetKeyMatrix.js
@@ -1,39 +1,39 @@
 /**
  * @swagger
- * /api/client/bu/dashboard/caseSheet/{clientId}/{buId}/{branchId}/{day}:
+ * /api/client/bu/dashboard/caseSheet:
  *   get:
  *     summary: Get Case Sheet Dashboard Data
  *     description: Retrieves case sheet dashboard statistics for a specific client, business unit, branch, and day.
  *     tags:
  *       - Admin Dashboard
  *     parameters:
- *       - in: path
+ *       - in: query
  *         name: clientId
  *         schema:
  *           type: string
  *         required: true
  *         description: Unique identifier for the client.
  *         example: "6782084da840f3a7bf1a2f72"
- *       - in: path
+ *       - in: query
  *         name: buId
  *         schema:
  *           type: string
- *         required: true
+
  *         description: Unique identifier for the business unit.
- *         example: "BU123"
- *       - in: path
+ *         example: "67820851a840f3a7bf1a307a"
+ *       - in: query
  *         name: branchId
  *         schema:
  *           type: string
- *         required: true
+
  *         description: Unique identifier for the branch.
- *         example: "BR001"
- *       - in: path
+ *         example: "6778e6e7b4f5258830c1c3d4"
+ *       - in: query
  *         name: day
  *         schema:
  *           type: string
  *           format: date
- *         required: true
+ *        
  *         description: Specific day to retrieve dashboard data for (YYYY-MM-DD).
  *         example: "2025-04-28"
  *     responses:
@@ -42,23 +42,24 @@
  *         content:
  *           application/json:
  *             example:
+ *               status: true
+ *               message: "CaseSheet counts fetched successfully!"
  *               data:
  *                 - Type: "Total Cases"
- *                   value: 400
+ *                   value: 1
  *                 - Type: "Proposed"
- *                   value: "400"
- *                 - Type: "in-Progress"
- *                   value: 40
+ *                   value: 0
+ *                 - Type: "In Progress"
+ *                   value: 0
  *                 - Type: "Completed"
- *                   value: 400
+ *                   value: 1
  *                 - Type: "Cancelled"
- *                   value: 100
+ *                   value: 0
  *               metaData:
  *                 day: "2025-04-28"
  *                 totalCount: 1
- *             
  *       400:
- *         description: Invalid or missing path parameters.
+ *         description: Invalid or missing query parameters.
  *         content:
  *           application/json:
  *             example:
--- documentation/getPerformanceOverview.js
+++ documentation/getPerformanceOverview.js
@@ -1,47 +1,49 @@
 /**
  * @swagger
- * /api/client/bu/dashboard/performance/{clientId}/{buId}/{branchId}/{day}:
+ * /api/client/bu/dashboard/performance:
  *   get:
  *     summary: Get Dashboard Performance Data
  *     description: Retrieves the performance data including Chair Occupancy Rate, Avg. Inpatient TAT, Registered Employees, and Services Count for a specific client, business unit, branch, and day.
  *     tags:
  *       - Admin Dashboard
  *     parameters:
- *       - in: path
+ *       - in: query
  *         name: clientId
  *         schema:
  *           type: string
  *         required: true
  *         description: Unique identifier for the client.
  *         example: "6782084da840f3a7bf1a2f72"
- *       - in: path
+ *       - in: query
  *         name: buId
  *         schema:
  *           type: string
- *         required: true
+
  *         description: Unique identifier for the business unit.
- *         example: "BU123"
- *       - in: path
+ *         example: "67820851a840f3a7bf1a307a"
+ *       - in: query
  *         name: branchId
  *         schema:
  *           type: string
- *         required: true
+
  *         description: Unique identifier for the branch.
- *         example: "BR001"
- *       - in: path
+ *         example: "6778e6e7b4f5258830c1c3d4"
+ *       - in: query
  *         name: day
  *         schema:
  *           type: string
  *           format: date
- *         required: true
+
  *         description: Specific day to retrieve the performance data for (YYYY-MM-DD).
- *         example: "2025-04-28"
+ *         example: "2025-04-27"
  *     responses:
  *       200:
  *         description: Performance data retrieved successfully.
  *         content:
  *           application/json:
  *             example:
+ *               status: 200
+ *               message: "CaseSheets counts fetched successfully"
  *               data:
  *                 - Module: "Chair"
  *                   Message: "Chair Occupancy Rate"
@@ -56,16 +58,16 @@
  *                   Message: "Services Count"
  *                   value: "11"
  *               metaData:
- *                 day: "2025-04-28"
- *                 totalCount: 1
+ *                 day: "2025-04-27"
+ *                 totalCount: 100
  *       400:
- *         description: Invalid or missing path parameters.
+ *         description: Invalid or missing query parameters.
  *         content:
  *           application/json:
  *             example:
  *               status: false
  *               statusCode: 400
- *               message: "Invalid or missing path parameters."
+ *               message: "Invalid or missing parameters."
  *       500:
  *         description: Internal server error.
  *         content:
