921ce86f7a3b14a25f1d1fd0a01db44fe8c6fe35
sandeeppachatgmailcom <sandeeppachat@gmail.com>
2025-05-14 17:49:28 +0530

    dummy api for dashboard revenue summary added and tested

--- businessUnitAdministration/routes/dashboardRouter.routes.js
+++ businessUnitAdministration/routes/dashboardRouter.routes.js
@@ -3,12 +3,14 @@ const getCaseSheetDashboardCTRL = require("../controller/dashboard/getCaseSheetD
 const getMetricsDashboardCTRL = require("../controller/dashboard/getMetricsDashboardCTRL");
 const getGraphDashboardCTRL = require("../controller/dashboard/getGraphDashboardCTRL");
 const getPerformanceDashboardCTRL = require("../controller/dashboard/getPerformanceDashboardCTRL");
+const getRevenueSummaryCTRL = require("../controller/dashboard/getRevenueSummaryCTRL");
 
 const dashboardRouter = express.Router();
 
 dashboardRouter.get("/caseSheet", getCaseSheetDashboardCTRL);
 dashboardRouter.get("/metrics", getMetricsDashboardCTRL);
 dashboardRouter.get("/graph", getGraphDashboardCTRL);
-dashboardRouter.get("/performance", getPerformanceDashboardCTRL);
+dashboardRouter.get("/performance", getPerformanceDashboardCTRL)
+.get('/revenueSummary', getRevenueSummaryCTRL)
 
 module.exports = dashboardRouter
